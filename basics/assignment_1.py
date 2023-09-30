587e24691907f1e2da6e9ab65431da3ff77cd35d
Alina Violeta Miu <AlinaVioMiu>
2023-10-01 01:59:51 +0300

    update

--- basics/assignment_1.py
+++ basics/assignment_1.py
@@ -1,19 +1,19 @@
 # DRIVERS LICENCE APPLICATION
-first_name = 'Alina'
-last_name = 'Miu'
-age = 34
-cnp = 123456789
-height = 1.59
-weight = 60.0
-print(first_name, last_name, age, cnp, height, weight)
+first_name = input('first name = ')
+last_name = input('last name = ')
+age = int(input('age = '))
+ssn = int(input('ssn = '))
+height = float(input('height = '))
+weight = float(input('weight = '))
+print(first_name, last_name, age, ssn, height, weight)
 
 first_name = input("Enter your first name: ").title()
 second_name = input("Enter your second name: ").title()
 age = int(input("Enter your age:"))
 if age >= 18:
-   social_security_number = int(input("Enter your social security number: "))
-   height = float(input("Enter your height in cm: "))
-   weight = int(input("Enter your weight in kg: "))
+    social_security_number = int(input("Enter your social security number: "))
+    height = float(input("Enter your height in cm: "))
+    weight = int(input("Enter your weight in kg: "))
 
 else:
     print("Sorry, you are not eligible to apply for driving license")
