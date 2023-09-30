024dd13c85c6d5d946863f36ede540f31f322f89
Alina Violeta Miu <AlinaVioMiu>
2023-10-01 01:49:21 +0300

    first commit

---
+++ basics/hello.py
@@ -0,0 +1,9 @@
+# first lines
+'''multi-line comments
+-------------------
+'''
+print('Python is easy!')
+# indentation
+# 4 spaces of indentation - most common in python for a block of code
+
+# data type
---
+++ basics/numeric_types.py
@@ -0,0 +1,46 @@
+# integers
+a = 13
+b = 100
+c = -66
+print(a, b, c)
+print(type(a))
+
+# float
+x = 45.66
+y = -123.4
+z = 205.0
+print(x, y, z)
+print(type(z))
+
+# complex
+d = 3 + 5j
+print(d)
+print(type(d))
+
+# binary type
+e = 0B1010
+print(e)
+print(type(e))
+# hexadecimal
+f = 0xFF
+print(f)
+print(type(f))
+
+# to convert a octa number you will use 0 to define a Octa decimal so use 0 0.
+
+# boolean type
+g = True
+print(type(g))
+print(9>8)
+print(9<8)
+
+h = int(x)
+print(h)
+print(type(h))
+i = float('22.5')
+print(type(i))
+print(bin(10))
+print(hex(10))
+print(oct(10))
+
+# IDENTIFIERS = The names we give to the variables. case sensitive!!!
